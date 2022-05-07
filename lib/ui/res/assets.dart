2ddf5d7b9ee411a60bf0451e9a4fcf5eea623473
aiserrock <aicserrock@gmail.com>
2022-05-07 16:12:41 +0300

    4-10 added icons

---
+++ lib/ui/res/assets.dart
@@ -0,0 +1,8 @@
+class IconRes{
+  const IconRes._();
+
+  static const String _baseIconPath = 'res/icon';
+
+  static const icHeart = '$_baseIconPath/ic_heart.svg',
+      icCalendar = '$_baseIconPath/ic_calendar.svg';
+}
